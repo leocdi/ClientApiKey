591dcfa86e6013bbdcce03b7007212f54560c048
leocdi <leomauv@gmail.com>
2023-05-22 22:11:44 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication30/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Net.Http.Headers;
+using WebClientApiKey;
+
+namespace WebApplication30.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IswaggerClient _swaggerClient;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IswaggerClient swagger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _swaggerClient = swagger;
+        }
+
+        public async Task OnGet()
+        {
+            //var headerName = "X-Api-Key";
+            //var headerValue = _configuration.GetValue<string>("ApiKey");
+
+            //using var httpClient = new HttpClient();
+            //httpClient.DefaultRequestHeaders.Add(headerName, headerValue);
+
+           // var api = new swaggerClient("https://localhost:7071/", httpClient);
+            try
+            {
+                var z = await _swaggerClient.GetWeatherForecastAsync();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+         
+        }
+    }
+}
