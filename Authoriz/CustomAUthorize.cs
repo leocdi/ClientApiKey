9d78f1fb12f70f5c12052388e05a8000114622b7
leocdi <leomauv@gmail.com>
2023-07-03 08:13:32 +0200

    exemple page 403

---
+++ Authoriz/CustomAUthorize.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Principal;
+
+namespace Authoriz
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class CustomAuthorizationAttribute : AuthorizeAttribute
+    {
+
+
+
+    }
+}
---
+++ Authoriz/MyClaimsTransformation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+public class MyClaimsTransformation : IClaimsTransformation
+{
+
+    public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+        var claimType = "myNewClaim";
+        if (!principal.HasClaim(claim => claim.Type == claimType))
+        {
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+        }
+
+        principal.AddIdentity(claimsIdentity);
+        return Task.FromResult(principal);
+    }
+
+}
